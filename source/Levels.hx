3ff442c5763b9ba4c0b2770d2104654c0a5e19ad
Tom Blount <tb12g09@ecs.soton.ac.uk>
2015-07-30 15:32:11 +0100

    Changed timers

--- source/Levels.hx
+++ source/Levels.hx
@@ -110,7 +110,7 @@ class Levels {
                     "Next"
                 ],
 
-                99999
+                120
             ),
             new Level(
                 "Great Scott! Lord Wafflington, world renowned explorer, has come to our hospital complaining of stomach pains after a trip to Columbia! We'd better make sure to fix him up to the highest of standards or our reputation will suffer",
@@ -148,7 +148,7 @@ class Levels {
                     "PressureGauge"
                 ],
 
-                99999
+                120
             ),
             new Level(
                 "Now its time to get cutting!",
@@ -177,7 +177,7 @@ class Levels {
                     "Scalpel"
                 ],
 
-                99999
+                120
             ),
         new Level(
                 "The queues are mounting up around the block! There's nothing else for it; get in there and help those people. You've only got a few seconds to spend fixing each patient: take too long and they'll kick the bucket. Even worse, that patient won't count towards finishing your shift! HINT: Press spacebar to quickly get the next patient",
@@ -196,7 +196,7 @@ class Levels {
                     "Clock"
                 ],
 
-                10
+                60
             )
         ];
     }
