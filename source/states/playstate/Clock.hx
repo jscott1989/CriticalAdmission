214d0cfe4182337cc3ed80a8471947b4c5d96e6e
Tom Blount <tb12g09@ecs.soton.ac.uk>
2015-07-31 14:39:12 +0100

    Added red flash at < 5 seconds

--- source/states/playstate/Clock.hx
+++ source/states/playstate/Clock.hx
@@ -1,8 +1,10 @@
 package states.playstate;
 
- import flixel.text.FlxText;
- import flixel.util.FlxColor;
-
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.effects.FlxSpriteFilter;
+import flash.filters.GlowFilter;
+import flixel.tweens.FlxTween;
 /**
  * Clock that shows the current time left in the level
  */
@@ -12,6 +14,10 @@ package states.playstate;
     public static inline var TEXT_Y_OFFSET = 70;
 
     private var text:FlxText;
+
+    private var filter:FlxSpriteFilter;
+    private var redFilter:GlowFilter;
+    private var filterTween:FlxTween;
 
     public function new()  {
         super("Clock");
@@ -23,6 +29,10 @@ package states.playstate;
         text.alignment = "center";
         text.color = FlxColor.BLACK;
         text.size = 40;
+
+        filter = new FlxSpriteFilter(this);
+        redFilter = new GlowFilter(0xFF0000, 1, 50, 50, 1.5, 1);
+        filterTween = FlxTween.tween(redFilter, { blurX: 4, blurY: 4 }, 1, { type: FlxTween.PINGPONG });
 
         updateText();
         PlayState.getInstance().add(text);
@@ -43,17 +53,34 @@ package states.playstate;
         }
     }
 
+    /**
+     * Flash red if below 5 seconds
+     */
+    private function updateClock() {
+        var seconds_remaining = PlayState.getInstance().seconds_remaining;
+        if (seconds_remaining <= 5) {
+            if(filter.filters.length == 0){
+                filter.addFilter(redFilter);
+            }
+        }
+        else{
+            filter.removeFilter(redFilter);
+        }
+
+        filter.applyFilters();
+    }
+
     public override function update() {
         text.x = x + TEXT_X_OFFSET;
         text.y = y + TEXT_Y_OFFSET;
 
-        // Always ensure that the text is in front of us in the state
-
-        Utils.bringToFront(PlayState.getInstance().members, text, this);
-
         // TODO: Adjust font size to match scale of clock
 
+        updateClock();
         updateText();
+
+        // Always ensure that the text is in front of us in the state
+        Utils.bringToFront(PlayState.getInstance().members, text, this);
     }
 
     private function timeUp() {
